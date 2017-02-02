d7c028b6f0ac9ee8bd6716a0bc6b5dc678c08522
雀乃丸焼pro <apple@szmnmryk.int.kcg.edu>
2017-02-02 10:59:19 +0900

    master作成

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sequel'
+db_path = File.dirname(__FILE__). + "db.db"
+DB = Sequel.sqlite(db_path)
+
+get '/' do
+  @title = "rootpage"
+  erb :index
+end
+
+get '/login' do
+
+end
