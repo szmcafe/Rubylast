ecf732efa29ed29f22ff759bf50eba50e08e61c5
雀乃丸焼pro <apple@szmnmryk.int.kcg.edu>
2017-02-02 15:32:30 +0900

    DBとの接続に成功

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 require 'sequel'
 
 use Rack::Session::Cookie,
@@ -7,22 +8,36 @@ use Rack::Session::Cookie,
   :secret => 'change'
 
 
-db_path = File.dirname(__FILE__). + "db.db"
+options = {:encoding=>"utf8"}
+db_path = File.dirname(__FILE__). + "/db/db.db"
+
 DB = Sequel.sqlite(db_path)
 
-
 get '/' do
+  @members = DB[:user]
   @title = "rootpage"
   erb :index
 end
 
 post '/' do
+  @members = DB[:user]
+  @title = "rootpage"
   session[:userID] = params[:userID]
   session[:userName] = params[:username]
   erb :index
+
 end
 
 get '/login' do
+    @members = DB[:user]
     @title = "loginpage"
     erb :login
+end
+
+post '/logout' do
+  @members = DB[:user]
+  @title = "logout"
+  session[:userID] = ""
+  session[:userName] = ""
+  erb :index
 end
