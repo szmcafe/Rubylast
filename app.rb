783629b55f7f22f962a4e4029806d7c0ef004ebc
雀乃丸焼pro <apple@szmnmryk.int.kcg.edu>
2017-02-02 11:31:55 +0900

    テンプレート更新

--- app.rb
+++ app.rb
@@ -4,11 +4,20 @@ require 'sequel'
 db_path = File.dirname(__FILE__). + "db.db"
 DB = Sequel.sqlite(db_path)
 
+
 get '/' do
   @title = "rootpage"
   erb :index
 end
 
+post '/' do
+  @user['name'] = params[:username]
+  @user['userID'] = params[:userID]
+  erb :index
+end
+
 get '/login' do
-  
+    @user = Hash.new()
+    @title = "loginpage"
+    erb :login
 end
